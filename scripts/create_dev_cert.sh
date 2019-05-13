aef4a586aee685f54a33ad929057d25121f5b92c
Doug Anson <doug.anson@arm.com>
2019-05-13 10:47:17 -0500

    v1.0 mbed-edge enabled bridge to edgex

--- scripts/create_dev_cert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-API_KEY=$1
-CERT_NAME=$2
-CERT_DESC=$3
-
-curl -s -X POST "https://api.us-east-1.mbedcloud.com/v3/developer-certificates" -H "accept: application/json" -H "Authorization: Bearer ${API_KEY}" -H "content-type: application/json" -d "{ "\"name\"": "\"${CERT_NAME}\"", "\"description\"": "\"${CERT_DESC}\""}" | jq .id
--- scripts/get_cert_id.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-API_KEY=$1
-
-curl -s -X GET "https://api.us-east-1.mbedcloud.com/v3/trusted-certificates" -H "accept: application/json" -H "Authorization: Bearer ${API_KEY}" -H "content-type: application/json" | jq '.data[] | select(.name=="EdgeXCert") | .id'
